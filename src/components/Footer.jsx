2a84254ca23b519276ad99044438df26b4766613
Rohit Rathod <56649465+rohitrathod013@users.noreply.github.com>
2025-01-26 15:12:54 -0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className ="text-center">
+                        <p className="footer-text">Copyright 2025 &copy; Rohit Rathod</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
